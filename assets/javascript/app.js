72211d434d80d366c3e78f073f433f28326d0897
Joe Mankin <joseaphmankin@gmail.com>
2018-08-24 11:49:50 -0500

    Initial Commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+// Start Button to get game going
+// Question presented w/ 4 options. timer starts
+// answer picked. right answer shown for 5 seconds
+// Next question presented. repeat for x questions.
+// After all questions asked, present Right/Wrong/Didn't answer. Restart button
